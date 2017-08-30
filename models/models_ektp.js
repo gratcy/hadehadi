90cbc6806636892b4affd760d67421ba2c4ae08a
gratcy <palmagratcy@gmail.com>
2017-08-31 01:39:04 +0700

    wew

--- models/models_ektp.js
+++ models/models_ektp.js
@@ -2,25 +2,33 @@ var exports = module.exports = {};
 
 exports.check_ektp = function(nik) {
     var deferred = q.defer();
-	sql.connect(dbConf).then(pool => {
-		return pool.request().query("SELECT * FROM ektp WHERE nik='"+nik+"'")
-	}).then(result => {
-		deferred.resolve(result);
-	}).catch(err => {
-		deferred.reject(err);
-	})
+	sql.connect(dbConf, function (err) {
+		var request = new sql.Request();
+		request.query("SELECT * FROM ektp WHERE nik='"+nik+"'", function (err, result) {
+			if (err) {
+				deferred.reject(err);
+			}
+			else {
+				deferred.resolve(result);
+			}
+		});
+	});
     return deferred.promise;
 };
 
 exports.get_ektp = function() {
     var deferred = q.defer();
-	sql.connect(dbConf).then(pool => {
-		return pool.request().query("SELECT * FROM ektp")
-	}).then(result => {
-		deferred.resolve(result);
-	}).catch(err => {
-		deferred.reject(err);
-	})
+	sql.connect(dbConf, function (err) {
+		var request = new sql.Request();
+		request.query("SELECT * FROM ektp", function (err, result) {
+			if (err) {
+				deferred.reject(err);
+			}
+			else {
+				deferred.resolve(result);
+			}
+		});
+	});
     return deferred.promise;
 };
 
