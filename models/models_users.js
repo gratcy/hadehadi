905d3cd3e5a1b61f0370460ac0d91c68629013eb
gratcy <palmagratcy@gmail.com>
2017-08-31 01:01:46 +0700

    wew

--- models/models_users.js
+++ models/models_users.js
@@ -5,8 +5,10 @@ exports.get_users = function(username,pass) {
 	sql.connect(dbConf).then(pool => {
 		return pool.request().query("SELECT email,username,pass,fullname,create_date,access_date FROM users WHERE username='"+username+"' AND pass='"+helpers.generateHash(pass)+"'")
 	}).then(result => {
+		sql.close();
 		deferred.resolve(result);
 	}).catch(err => {
+		sql.close();
 		deferred.reject(err);
 	})
     return deferred.promise;
