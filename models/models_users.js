e4f89a5527f68c2dae03b8736a3324fd3aa075b2
unknown <cybertank378>
2017-12-07 00:44:46 +0700

    Adding macaddres input while check user is exist

--- models/models_users.js
+++ models/models_users.js
@@ -1,14 +1,41 @@
 var exports = module.exports = {};
 
-exports.get_users = function(username,pass) {
+exports.get_users = function(username,pass,macaddr) {
     var deferred = q.defer();
 	var pool = sql.connect(dbConf, function (err) {
 		var request = new sql.Request();
 		request.query("SELECT * FROM users WHERE username='"+username+"' AND pass='"+helpers.generateHash(pass)+"'", function (err, result) {
 			if (err) {
 				deferred.reject(err);
+			}else {
+				deferred.resolve(result);
+				var request = new sql.Request();
+				request.query("UPDATE [users] SET macaddr = '"+macaddr+"' WHERE username='"+username+"' AND pass='"+helpers.generateHash(pass)+"", function (err, result) {
+					if (err) {
+						deferred.reject(err);
+					}else {
+						deferred.resolve(result);
+					}
+				});
+
 			}
-			else {
+		});
+	});
+	sql.on('error', err => {
+		console.log(err);
+	})
+    return deferred.promise;
+};
+
+
+exports.update_users = function(username,pass,macaddr) {
+    var deferred = q.defer();
+	var pool = sql.connect(dbConf, function (err) {
+		var request = new sql.Request();
+		request.query("UPDATE [users] SET macaddr = '"+macaddr+"' WHERE username='"+username+"' AND pass='"+helpers.generateHash(pass)+"", function (err, result) {
+			if (err) {
+				deferred.reject(err);
+			}else {
 				deferred.resolve(result);
 			}
 		});
--- routes/users.js
+++ routes/users.js
@@ -9,56 +9,25 @@ exports.get_users = async function(req, res) {
 	try {
 		if (!username || !pass || !macaddr) {
 			res.send({error: {status: -1}, message: 'Input incomplete!'});
-		}
-		else {
-			if (sql.connect) sql.close();
-			var rows = await models_users.get_users(username,pass,macaddr);
-			var data = rows.recordsets[0];
-			sql.close();
-			if (data.length > 0)
-				import models_users from '../models/models_users';
-
-exports.get_users = async function(req, res) {
-	var input = req.body;
-	var username = helpers.decryptAES(input.username);
-	var pass = helpers.decryptAES(input.pass);
-	var macaddr = helpers.decryptAES(input.macaddr);
-	
-	try {
-		if (!username || !pass || !macaddr) {
-			res.send({error: {status: -1}, message: 'Input incomplete!'});
 		} else {
 			if (sql.connect) sql.close();
+
+
 			var rows = await models_users.get_users(username,pass);
 			
 			var data = rows.recordsets[0];
-			
 			sql.close();
-			if (data.length > 0)
-				var request = new sql.Request();
-				request.query("insert into users (macaddr) values ('"+macaddr+"') where  (username,pass) values('"+username+"','"+pass+"')", function (err2, result2) {
-					if (err2) {
-						res.send({error: {status: -1}, message: 'Failed insert log data!',err2});
-					}
-					else {
-						console.log('Success insert log data!');
-						res.send({error: {status: 1}, message: 'Success insert log data!'});
-					}
-				});
-
+			if (data.length > 0 ){
+				
 				res.send({error: {status: 1}, users: rows.recordsets[0]});
-			else
-				res.send({error: {status: -1}, message: 'Username and password not match !'});
-		}
-	}
-	catch (error){
-		console.log(error);
-	}
-};
+			}else{
 
-				res.send({error: {status: 1}, users: rows.recordsets[0]});
-			else
 				res.send({error: {status: -1}, message: 'Username and password not match !'});
+
+			}
+				
+			
+				
 		}
 	}
 	catch (error){
